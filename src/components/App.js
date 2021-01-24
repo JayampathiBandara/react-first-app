4fa2e0ae012ed6af67b10635441bf8252dd94ab3
jayampathib <wmjb.weerasinghe@gmail.com>
2021-01-24 12:03:30 +0800

    8.3	Add routing configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import HomePage from "./HomePage";
-import AboutPage from "./AboutPage"; 
+import AboutPage from "./AboutPage";
 import Header from "./common/Header";
-import CoursesPage from "./CoursesPage";
+import CoursesPage from "./CoursesPage"; // With default export, import is easy
+import { Route } from "react-router-dom"; // without default export, , import is inside {}
 
 function App() {
+  /* no need with Route
   function getPage() {
     const route = window.location.pathname;
     if (route === "/courses") return <CoursesPage />;
     if (route === "/about") return <AboutPage />;
     return <HomePage />;
-  }
+  }*/
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Route path="/" exact component={HomePage} />
+      <Route path="/courses" component={CoursesPage} />
+      <Route path="/about" component={AboutPage} />
     </div>
   );
 }
-
 
 export default App;
