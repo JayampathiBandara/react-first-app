427df1a7e71bfaaaba0dc29b4fed9a8f1f3e4478
jayampathib <wmjb.weerasinghe@gmail.com>
2021-01-24 12:24:19 +0800

    8.4.1	Link - add link navigation without full page refresh

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="jumbotron">
       <h1>Pluralsight Administration</h1>
       <p>React, Flux, and React Router for ultra-responsive web apps.</p>
-      <a href="/about">About</a>
+      {/*  <a href="/about">About</a> */}
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 }
-// traditionally, if only a single item is being exported from a file, you will use a default export. 
+// traditionally, if only a single item is being exported from a file, you will use a default export.
 // The benefit of a default export is  requires a little less code to import it
 // and the file doing the import can decide what to name that import
 export default HomePage;
