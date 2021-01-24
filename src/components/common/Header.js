b08fdf82b7ed2170ed3d54b5e6b74c95d38fc740
jayampathib <wmjb.weerasinghe@gmail.com>
2021-01-24 12:34:28 +0800

    8.4.2	NavLink added to main menu – has additional parameter active class name

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Header() {
+  const activeStyle = { color: "orange" };
   return (
     <nav>
-      <a href="/">Home</a> | <a href="/about">About</a> | <a href="/courses">Courses</a>
+      <NavLink to="/" activeStyle={activeStyle} exact>
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStyle}>
+        About
+      </NavLink>
+      {" | "}
+      <NavLink to="/courses" activeStyle={activeStyle}>
+        Courses
+      </NavLink>
     </nav>
   );
 }
+
+/* function Header() {
+  return (
+    <nav>
+      <a href="/">Home</a> | <a href="/about">About</a> |{" "}
+      <a href="/courses">Courses</a>
+    </nav>
+  );
+} */
 
 export default Header;
